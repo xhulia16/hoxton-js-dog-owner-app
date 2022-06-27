a4331cf12f6726ad25e8571ac054620a6737d9c3
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-27 02:12:56 +0200

    displaty card

--- src/index.js
+++ src/index.js
@@ -6,25 +6,62 @@
 //- The card should have a button that toggles for the selected dog between good dog/ bad do//g
 console.log(data);
 
-
 function createDogItemList(dog) {
 
- let dogNameLi=document.createElement("li")
- dogNameLi.className="dogs-list__button"
- dogNameLi.textContent=dog.name
+    let dogNameLi = document.createElement("li")
+    dogNameLi.className = "dogs-list__button"
+    dogNameLi.textContent = dog.name
+    dogNameLi.addEventListener("click", function(){
+        displayDog(dog)
+    } )
 
-let dogList=document.querySelector('.dogs-list')
-dogList.append(dogNameLi)
-console.log(dogList)
+    let dogList = document.querySelector('.dogs-list')
+    dogList.append(dogNameLi)
+    console.log(dogList)
+
 }
 
 
-for(let dog of data){
-    createDogItemList(dog) }
+for (let dog of data) {
+    createDogItemList(dog)
+}
+//<h2>Mr. Bonkers</h2>
+ //       <img
+ //         src="https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_1.jpg"
+// //         alt=""
+  //      />
+        //<div class="main__dog-section__desc">
+ //         <h3>Bio</h3>
+ //         <p>
+ //           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum,
+ //           minima voluptates libero cumque rerum consequatur optio aliquid sint
+ //           eum maxime illo laborum omnis quo ab rem cupiditate nulla
+ //           perspiciatis ipsum!
+  //        </p>
+  //      </div>
+  //      <p><em>Is naughty?</em> yes!</p>
+  //      <button class="main__dog-section__btn">Good dog!</button>
+//
+let dogCard = document.querySelector('.main__dog-section')
+console.log(dogCard)
 
-//let dogCard = document.querySelector('.main__dog-section')
-//console.log(dogCard)
+function displayDog(dog) {
+   dogCard.textContent = ''
 
-//function displayDog() {
-  //  dogCard.textContent = ''
-//}
+   let nameH2=document.createElement("h2")
+   nameH2.textContent=dog.name
+
+   let dogImg= document.createElement("img")
+   dogImg.src=dog.image
+
+   let dogP= document.createElement("p")
+   dogP.textContent= dog.bio
+
+   let dogBehaviourP= document.createElement("p")
+   dogBehaviourP.textContent= dog.isGoodDog
+   let dogMainCard= document.querySelector('.main__dog-section')
+   dogMainCard.append(nameH2, dogImg, dogP, dogBehaviourP)
+   console.log(dogMainCard)
+}
+
+//document.getElementById("myBtn").addEventListener("click", displayDog);
